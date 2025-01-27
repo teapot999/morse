cf3c4a900fa24b0a3e9542136234378b0199e94f
KHALTURIN <mothalk@yandex.ru>
2025-01-27 19:07:08 +0400

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello')
+MorseCode = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-..', 'G': '--.',
+             'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+             'P': '.--.',
+             'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+             'Y': '-.--', 'Z': '--..'}
+
+
+def encode_to_morse(text: str):
+    t = list(text.upper())
+    for i in range(len(t)):
+        if t[i] in MorseCode:
+            t[i] = MorseCode[t[i]]
+    return ''.join(t)
+
+
+print(encode_to_morse('te st'))  # => -. ...-
